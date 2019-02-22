a2fd88fc41145aae699c9994ab1b2855005ffee5
xod442 <rick@rickkauffman.com>
2019-02-22 20:53:07 +0000

    added instructions in py file

--- actions/myecho.py
+++ actions/myecho.py
@@ -2,6 +2,10 @@ import sys
 
 from st2common.runners.base_action import Action
 
+# class takes two variables, message (position 1) and other (position 2)
+
+# st2 run rick.echo_action message="message" other="other"
+
 class MyEchoAction(Action):
     def run(self, message, other):
         print(message)
